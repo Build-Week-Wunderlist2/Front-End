b77fc23355a13123d6625fc7dbf6badf60f1886a
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-06-18 00:12:53 -0400

    fixing axios

--- wunderlist/src/utils/axioswithAuth.js
+++ wunderlist/src/utils/axioswithAuth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosWithAuth = () => {
+export const axiosWithAuth = () => {
     const token = localStorage.getItem("token");
     return axios.create({
         baseURL: "",
@@ -10,4 +10,3 @@ const axiosWithAuth = () => {
     });
 }
 
-export default axiosWithAuth;
