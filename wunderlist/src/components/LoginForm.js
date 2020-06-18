4067c2941e97f615810160da3e2624398d7f8867
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-06-18 00:06:14 -0400

    missing semicolons?

--- wunderlist/src/components/LoginForm.js
+++ wunderlist/src/components/LoginForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import { Link, useHistory } from 'react-router-dom'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 import axiosWithAuth from '../utils/axiosWithAuth';
 import * as yup from 'yup';
 
