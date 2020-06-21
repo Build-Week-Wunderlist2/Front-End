988f4521ce15e0cbafe6a89cb736cac1d5c0ca22
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-06-21 13:05:55 -0400

    adding if statements for edits and todos and forms that will appear

--- wunderlist/src/Components/ToDoList.js
+++ wunderlist/src/Components/ToDoList.js
@@ -3,11 +3,127 @@ import axiosWithAuth from '../utils/axiosWithAuth';
 import { ToDoContext } from '../contexts/ToDoContext';
 import ToDoForm from './ToDoForm';
 
-
-const ToDoList =() => {
-    const [todo, setToDo] = useState([]);
-    const [editToDo, setEditToDo] = useState({});
-    const [formState, setFormState] = useState({
-        
-    })
+const initialToDo = {
+    title: '',
+    complete: false,
+    user_id: `${user_id}`,
 };
+
+const initialTask = {
+    description: '',
+    complete: false,
+    task_id: `${id}`,
+    repeatsDaily: false,
+    repeatsWeekly: false,
+    repeatsMonthly: false
+};
+
+const ToDoList =({todo, updateToDo}) => {
+    console.log(todo);
+    const [editing, setEditing] = useState(false);
+    const [editToDo, setEditToDo] = useState(initialToDo);
+    const [editTask, setEditTask] = useState(initialTask);
+
+    const editTodo = todo => {
+        setEditing(true);
+        setEditToDo(todo);
+    };
+
+    const saveToDo = e => {
+        e.preventDefault();
+        type === 'todo' ? 
+        axiosWithAuth()
+        .put(`/user/todos/${editTodo.id}`, editToDo)
+        .then(res=>{
+            console.log(res)
+            this.handleReload();
+        })
+        .catch(err=>{
+            console.error(
+                "ToDoList.js: saveToDo",
+                err.message,
+                err.response
+            );
+        }) : axiosWithAuth()
+        .put(`/user/task/${editTask.id}`, editTask)
+        .then(res=>{
+            console.log(res)
+            this.handleReload();
+        })
+        .catch(err=>console.error(err.message));
+    };
+
+    const deleteToDo = todo => {
+        type === 'todo' ? 
+        axiosWithAuth()
+        .delete(`user/todos/${todo.id}`)
+        .then(res=>{
+            setEditToDo(res.data);
+        })
+        .catch(err=>{
+            console.error(
+                "ToDoList.js: deleteToDo: todo",
+                err.message,
+                err.response
+            )}
+        ) : axiosWithAuth()
+        .delete(`user/${id}/task`)
+        .then(res=>{
+            setEditTask(res.data);
+        })
+        .catch(err=>{
+            console.error(
+                "ToDoList.js: deleteToDo: task",
+                err.message,
+                err.resposne
+            )
+        })
+    };
+
+    return(
+        <div className="todo-wrap">
+        <p>To Do's</p>
+        <ul>
+            {todos.map(todo=>(
+                <li key={todo.id} onClick={()=>editToDo(todo)}>
+                    <span>
+                        <span className="delete" onClick={e =>{
+                            e.stopPropagation();
+                            deleteToDo(todo)
+                        }}>
+                            Delete
+                        </span>{" "}
+                        {todo.title}
+                        {todo.complete}
+                        {todo.user_id}
+                    </span>
+                </li> 
+              
+            ))}
+        </ul>
+        {type ==='todo' ? editing && (
+            <form onSubmit={saveToDo}>
+                <legend>Edit To Do</legend>
+                <label>To Do:
+                    <input
+                    onChange={e=>
+                    setEditToDo({...editToDo, title: e.target.value})
+                }
+                value={editToDo.title}
+                    />
+                </label>
+            </form>
+        ) : editing && ( 
+        <form onSubmit ={saveToDo}>
+            <legend>Edit Task</legend>
+            <label>Task:
+                <input onChange ={e=>setEditTask({...editToDo,description: e.target.value})
+                }
+                value={editToDo.description}
+                />
+            </label>
+        </form>
+        )}
+
+    </div>
+    )};
