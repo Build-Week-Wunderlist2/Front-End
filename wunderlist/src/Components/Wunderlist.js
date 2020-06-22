2f50442fdfa99118d073c9613740bd8f6feb512a
Brandon Stine <you@example.com>
2020-06-22 01:42:23 -0400

    removed logs

--- wunderlist/src/Components/Wunderlist.js
+++ wunderlist/src/Components/Wunderlist.js
@@ -79,7 +79,6 @@ const Wunderlist = () => {
           const results = toDoList.filter(obj => {
             return obj.title.toLowerCase().includes(search.toLowerCase())
           })
-          console.log('how do i sort these',results.sort((a, b) => (a.title > b.title) ? 1 : -1))
           return setSearchResult(results)
         }
       }, [search]);
