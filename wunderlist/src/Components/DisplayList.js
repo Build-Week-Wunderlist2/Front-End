19eff5c9522b41eb396504db2c64840b09deac1c
Brandon Stine <you@example.com>
2020-06-24 23:38:37 -0400

    most of the responsive design complete

--- wunderlist/src/Components/DisplayList.js
+++ wunderlist/src/Components/DisplayList.js
@@ -1,95 +1,123 @@
+import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import EditTaskForm from "./EditTaskForm";
+import Switch from "./Switch";
+import styled from "styled-components";
+import {
+  DarkGold,
+  LightTan,
+  BurntOrange,
+  DarkPurple,
+  LightPurple,
+} from "../ColorPalette";
 
-import React, { useState } from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth'
-import EditTaskForm from './EditTaskForm';
-import Switch from './Switch';
-import styled from 'styled-components';
-import {DarkGold, LightTan, BurntOrange, DarkPurple, LightPurple} from '../ColorPalette'
-
-
-const completeBackground = 'rgba(64, 86, 161, .3)';
+const completeBackground = "rgba(64, 86, 161, .3)";
 
 const DisplayListContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: ${({complete}) => complete ? completeBackground : undefined};
-    margin: 1%;
-    border-radius: 15px;
-    max-height: 2em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ complete }) =>
+    complete ? completeBackground : undefined};
+  margin: 1%;
+  border-radius: 15px;
+  max-height: 2em;
+
+  p {
+    width: 100%;
+  }
+`;
+
+const DisplayHeaderContainer = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const DisplayListHeader = styled.h3`
-    margin-left: 1%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  margin: 0 0 0 1%;
 `;
 
+const DisplayList = ({ task, id, setRenderToDo, renderToDo }) => {
+  let updatedTask = {
+    id: id,
+    description: task.description,
+  };
 
-const DisplayList = ({task, id, setRenderToDo, renderToDo}) => {
+  const [editing, setEditing] = useState(false);
+  const [taskToEdit, setTaskToEdit] = useState(updatedTask);
 
-    let updatedTask = {
-        id: id,
-        description: task.description,
-        // complete: task.complete,
-        // repeatsDaily: task.repeatsDaily,
-        // repeatsWeekly: task.repeatsWeekly,
-        // repeatsMonthly: task.repeatsMonthly,
-     };
+  const editTask = () => {
+    setEditing(!editing);
+  };
 
-    const [editing, setEditing] = useState(false);
-    const [taskToEdit, setTaskToEdit] = useState(updatedTask);
+  const updateTask = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .put(`/user/task/${id}`, taskToEdit)
+      .then((res) => {
+        setRenderToDo(!renderToDo);
+        setEditing(false);
+      })
+      .catch((err) => {
+        console.log("catch for put", taskToEdit);
+        console.error(err.message, err.response);
+      });
+  };
 
-    const editTask = tasks => {
-        setEditing(!editing);
-    }
+  const deleteTask = () => {
+    axiosWithAuth()
+      .delete(`/user/task/${id}`)
+      .then((res) => {
+        setTaskToEdit(res.data);
+        setRenderToDo(!renderToDo);
+      })
+      .catch((err) => {
+        console.error(err.message, err.response);
+      });
+  };
 
-    const updateTask = (e) => {
-        e.preventDefault()
-        axiosWithAuth()
-        .put(`/user/task/${id}`, taskToEdit)
-        .then(res => {
-            setRenderToDo(!renderToDo);
-            setEditing(false);
-        })
-        .catch(err=> {
-            console.log('catch for put', taskToEdit)
-            console.error(err.message, err.response)
-        })
-    }
+  const taskRepeat =
+    task.repeatsDaily === true
+      ? "Daily"
+      : task.repeatsWeekly === true
+      ? "Weekly"
+      : task.repeatsMonthly === true
+      ? "Monthly"
+      : undefined;
+  return (
+    <>
+      <DisplayListContainer complete={task.complete}>
+        <DisplayHeaderContainer>
+          {editing === true ? (
+            <EditTaskForm
+              deleteTask={deleteTask}
+              editing={editing}
+              setEditing={setEditing}
+              updateTask={updateTask}
+              taskToEdit={taskToEdit}
+              setTaskToEdit={setTaskToEdit}
+              task={task}
+            />
+          ) : (
+            <DisplayListHeader onClick={() => editTask()}>
+              {task.description}
+            </DisplayListHeader>
+          )}
+          <p>{taskRepeat}</p>
+        </DisplayHeaderContainer>
+        <Switch
+          task={task}
+          id={id}
+          setRenderToDo={setRenderToDo}
+          renderToDo={renderToDo}
+        />
+      </DisplayListContainer>
+    </>
+  );
+};
 
-    const deleteTask = () => {
-        axiosWithAuth()
-        .delete(`/user/task/${id}`)
-        .then(res=>{
-            setTaskToEdit(res.data);
-            setRenderToDo(!renderToDo)
-        })
-        .catch(err=>{console.error(
-            err.message,err.response
-        )})
-    }
-
-    function handleChange(e){
-        e.stopPropagation(); 
-        setTaskToEdit({...task, [e.target.name]:e.target.checked})
-        
-    }
-    return (
-        <>
-        <DisplayListContainer complete={task.complete}>
-            <DisplayListHeader>{task.description}</DisplayListHeader>
-            <Switch task={task} id={id} setRenderToDo={setRenderToDo} renderToDo={renderToDo}/>
-        </DisplayListContainer>
-        {(editing === true ? <EditTaskForm editing={editing} setEditing={setEditing} updateTask={updateTask} taskToEdit={taskToEdit} setTaskToEdit={setTaskToEdit} task={task}/> : undefined
-      )}
-        <button style={{width: '25%'}}button="edit" onClick={()=>editTask(task)}>Edit</button>
-        <button style={{width: '25%'}} button="delete" onClick={e=>{
-            e.preventDefault();
-            deleteTask(task);
-        }}>Delete</button>
-            <ol>{task.repeatsDaily === true ? "This will run daily" : task.repeatsWeekly === true ? "This will run Weekly" : task.repeatsMonthly === true ? "This will run Monthly" : undefined }</ol>
-        </>            
-
-    )
-}
-
-export default DisplayList
+export default DisplayList;
--- wunderlist/src/Components/TaskForm.js
+++ wunderlist/src/Components/TaskForm.js
@@ -1,65 +1,87 @@
-import React, { useState } from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
-
+import React, { useState } from "react";
+import styled from 'styled-components'
+import axiosWithAuth from "../utils/axiosWithAuth";
+import * as yup from 'yup'
 
 const TaskForm = ({ id, renderToDo, setRenderToDo }) => {
-    let date = new Date().toLocaleString().split(',')[0];
+  let date = new Date().toLocaleString().split(",")[0];
 
-    const initialTask = {
-       task_id: id,
-       description: "",
-       complete: false, 
-       repeatsDaily: false,
-       repeatsWeekly: false,
-       repeatsMonthly: false,
-       created_at: date,
-    }
+  const initialTask = {
+    task_id: id,
+    description: "",
+    complete: false,
+    repeatsDaily: false,
+    repeatsWeekly: false,
+    repeatsMonthly: false,
+    created_at: date,
+  };
 
-    const [addTask, setAddTask] = useState(initialTask);
+  const [addTask, setAddTask] = useState(initialTask);
 
-    const saveTask = e => {
-     e.preventDefault()
-     axiosWithAuth()
-        .post('/user/task', addTask)
-        .then(res => {
-            console.log('taskform addtask', addTask);
-            setRenderToDo(!renderToDo)
-            setAddTask(initialTask)
-            console.log('my add task', addTask)
-        })
-        .catch(err => console.error(err.message, err.response))
+  const saveTask = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/user/task", addTask)
+      .then((res) => {
+        console.log("taskform addtask", addTask);
+        setRenderToDo(!renderToDo);
+        setAddTask(initialTask);
+        // console.log("my add task", addTask);
+      })
+      .catch((err) => console.error(err.message, err.response));
+  };
 
-}
+  const handleChange = (e) => {
+    e.preventDefault();
+    setAddTask({
+      ...addTask,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    });
+  };
 
-     const handleChange = e => {
-        e.preventDefault();
-       setAddTask({...addTask, [e.target.name]: (e.target.type==="checkbox" ? e.target.checked : e.target.value)});
-        }
-    
-    const handleSelection = (e) => {
-        const newSelection = e.target.value;
-        setAddTask({...addTask, [newSelection] : true});
+  const handleSelection = (e) => {
+    const newSelection = e.target.value;
+    setAddTask({ ...addTask, [newSelection]: true });
 
-        //possibly a conditional here to set the others back to false.
-        //could also build custom component for onClicks
-        //had a ton of issues with options not passing e.target.name
-    }
+    //possibly a conditional here to set the others back to false.
+    //could also build custom component for onClicks
+    //had a ton of issues with options not passing e.target.name
+  };
 
+  const TaskForms = styled.form`
+    display: flex;
+    justify-content: center;
+    margin: 1%;
+    border-radius: 15px;
+  `;
+  const TasksSelect= styled.select`
+     background: rgba(125, 125, 125, .1);
+     margin: 0 2%;
+  `;
 
-     return(
-        <>
-        <form onSubmit={saveTask}>
-            <input name='description' onChange={handleChange} value={addTask.description} placeholder='Name of Task'/>
-            <select onChange={(e) => handleSelection(e)}>
-                <option>Never</option>
-                <option value='repeatsDaily'>Daily</option>
-                <option value='repeatsWeekly'>Weekly</option>
-                <option value='repeatsMonthly'>Monthly</option>
-            </select>
-            <button type="submit">Add</button>
-        </form>
-        </>
-     )
-}
+  const TasksInput = styled.input`
+  
+  `;
+  return (
+    <>
+      <TaskForms onSubmit={saveTask}>
+        <input
+          name="description"
+          onChange={handleChange}
+          value={addTask.description}
+          placeholder="Name of Task"
+        />
+        <TasksSelect onChange={(e) => handleSelection(e)}>
+          <option>Repeats</option>
+          <option value="repeatsDaily">Daily</option>
+          <option value="repeatsWeekly">Weekly</option>
+          <option value="repeatsMonthly">Monthly</option>
+        </TasksSelect>
+        <button type="submit">Add</button>
+      </TaskForms>
+    </>
+  );
+};
 
 export default TaskForm;
